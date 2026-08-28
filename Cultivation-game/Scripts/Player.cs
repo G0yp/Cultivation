258700b8e1f46296b75af91f0e06db4a36db9aac
SamJAbott <abbottsam2006@gmail.com>
2026-08-27 20:21:54 -0500

    added basic movement sprint/walk changed res for project changed rendering for pixel art exported basespeed and sprint variables on base scene

--- Cultivation-game/Scripts/Player.cs
+++ Cultivation-game/Scripts/Player.cs
@@ -1,27 +1,38 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
-{
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+[Tool]
+public partial class Player : CharacterBody2D{
+	/// <summary>
+	/// Sets base speed of character before modification
+	/// </summary>
+	[Export]
+	public float BaseSpeed;
 
-	public override void _PhysicsProcess(double delta)
-	{
+	[Export]
+	public float Sprint;
+
+	float speed;
+
+	public override void _PhysicsProcess(double delta){
 		Vector2 velocity = Velocity;
 
+		if(Input.IsActionPressed("Sprint")){
+			speed = BaseSpeed + Sprint;
+		}
+		else {
+			speed = BaseSpeed;
+		}
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
+		if (direction != Vector2.Zero){
+			velocity.X = direction.X * speed;
+			velocity.Y = direction.Y * speed;
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		else{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, speed);
 		}
 
 		Velocity = velocity;
