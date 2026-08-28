97f05df1cf043160d6ee4f6f3b88109a10ccd101
SamJAbbott <abbottsam2006@gmail.com>
2026-08-27 21:27:14 -0500

    Movement (#19)
    Co-authored-by: goup <goupsoup2@gmail.com>

--- Cultivation-game/Scripts/Player.cs
+++ Cultivation-game/Scripts/Player.cs
@@ -1,29 +1,44 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
-{
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
+public partial class Player : CharacterBody2D{
+	// Base speed which gets modified
+	[Export]
+	public float BaseSpeed;
+	// Speed modifier
+	[Export]
+	public float Sprint;
+	[Export]
+	public float DashDistance;
+	// Internal speed
+	float speed;
+
+	public override void _PhysicsProcess(double delta){
 		Vector2 velocity = Velocity;
 
+		if(Input.IsActionPressed("Sprint")){
+			speed = BaseSpeed + Sprint;
+		}
+		else {
+			speed = BaseSpeed;
+		}
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
+		if (direction != Vector2.Zero){
+			velocity.X = direction.X * speed;
+			velocity.Y = direction.Y * speed;
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		else{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, speed);
 		}
-
+		if(Input.IsActionJustPressed("Dash")){
+			if (direction != Vector2.Zero){
+				Position += direction * DashDistance;
+			}
+		}
 		Velocity = velocity;
 		MoveAndSlide();
 	}
